e306ecf76aa19bd17fc180dd62ba64d0b53b8dfc
dimon <xlxdimsanxlx@gmail.com>
2021-09-11 15:20:15 +0300

    two

--- lesson/lms/students.py
+++ lesson/lms/students.py
@@ -4,7 +4,8 @@ STUDENTS = []
 
 TEST_STUDENTS = [
     ['Mari', 'D', 'adadad@mail.ru', '19', 'Huston', 'F'],
-    ['Jon', 's', 'hyhyhyhyh@mail.com', '21', 'London', 'M']
+    ['Jon', 's', 'hyhyhyhyh@mail.com', '21', 'London', 'M'],
+    ['Andy', 'H', 'ggggggg@mail.com', 'sexteen', 'Citi', 'M']
 ]
 
 
@@ -23,10 +24,22 @@ def load_students():
     for  test_student in TEST_STUDENTS:
         STUDENTS.append(dict(zip(student_fields, test_student)))
 
+def calculate_avg_age():
+    try:
+        total_age = 0
+        for student in STUDENTS:
+            total_age += int(student['age'])
+        avg_age = total_age / len(STUDENTS)
+        print(f'Average age is  {avg_age}')
+    except:
+        print('Cannot calculate average age')
+
 while True:
     action = input('Desired action:\t')
     if action == 'add':
         add_student()
+    elif action == 'age':
+        calculate_avg_age()
     elif action == 'load':
         load_students()
     elif action == 'print':
