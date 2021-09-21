b3c759c728181e8a13bd88007c2344dc27dec653
dimon <xlxdimsanxlx@gmail.com>
2021-09-21 19:14:05 +0300

    two

--- homework/test.py
+++ homework/test.py
@@ -1,5 +1,21 @@
-a = ['1', '2']
-b = ['1', '2']
+class Asdf:
 
-if a == b:
-    print('ok')
+    def __init__(self):
+        self.a = 21
+        self.b = 'ew'
+
+    def __str__(self):
+        return f'{self.a} {self.b}'
+
+class Dfdfgff(Asdf):
+
+    def __init__(self):
+        super()
+
+    def print_aaaa(self):
+        print(super(Asdf, self))
+
+
+a = Asdf()
+b = Dfdfgff()
+b.print_aaaa()
--- lesson/lms/students_oop.py
+++ lesson/lms/students_oop.py
@@ -3,46 +3,49 @@ import json
 class Student:
 
     student_fields = ['first_name', 'last_name', 'email', 'age', 'address', 'gender', 'point']
-    student = {}
 
     def __init__(self):
-        for field in self.student_fields:
-            self.student[field] = input(f'Enter {field}: ')
-            if field == 'age':
-                try:
-                    int(self.student['age'])
-                except:
-                    self.student['age'] = input('Enter age as number\t')
+        self. first_name = input(f'Enter {self.student_fields[0].replace("_", " ").capitalize()}:\t')
+        self. last_name = input(f'Enter {self.student_fields[1].replace("_", " ").capitalize()}:\t')
+        self. email = input(f'Enter {self.student_fields[2].replace("_", " ").capitalize()}:\t')
+        self.age = input(f'Enter {self.student_fields[3].replace("_", " ").capitalize()}:\t')
+        self. address = input(f'Enter {self.student_fields[4].replace("_", " ").capitalize()}:\t')
+        self.gender = input(f'Enter {self.student_fields[5].replace("_", " ").capitalize()}:\t')
+        self.point = input(f'Enter {self.student_fields[6].replace("_", " ").capitalize()}:\t')
 
 
-    def print_student(self, student):
-        for field in student:
-            print(field.replace('_', ' ').capitalize(), '\t', student[field])
+    def __str__(self):
+        return f'{self.first_name.capitalize()} {self.last_name.capitalize()}'
 
 
 class Group(Student):
+    '''
     _TEST_STUDENTS = [
         ['Mari', 'D', 'adadad@mail.ru', '19', 'Huston', 'F', 0],
         ['Jon', 's', 'hyhyhyhyh@mail.com', '21', 'London', 'M', 0],
         ['Andy', 'H', 'ggggggg@mail.com', 'sexteen', 'Citi', 'M', 0]
     ]
+    '''
 
     def __init__(self, name):
+        super()
         self.name = name
         self.STUDENTS = []
 
     def add_student_to_group(self, student):
         self.STUDENTS.append(student)
 
+    '''
     def load_students(self):
         for test_student in self._TEST_STUDENTS:
             self.STUDENTS.append(dict(zip(self.student_fields, test_student)))
+    '''
 
     def print_student_list(self):
         for student in self.STUDENTS:
-            Student.print_student(self, student)
+            print(student)
             print('<-------------------------------------------------------->')
-
+    '''
     def dump_students(self):
         with open('data/student_data_oop.json', 'w') as file:
             json.dump(self.STUDENTS, file)
@@ -50,17 +53,20 @@ class Group(Student):
     def load_from_json(self, file_path='data/student_data_oop.json'):
         with open(file_path, 'r') as file:
             self.STUDENTS.extend(json.load(file))
-
+    '''
 
 if __name__ == '__main__':
-    student = Student()
-    group = Group('beetroot')
-
-    student.print_student(student.student)
-
+    student1 = Student()
+    student2 = Student()
+    #print(student)
+    print('-------------------------------------------')
+    group = Group('Beetroot')
+    group.add_student_to_group(student1)
+    group.add_student_to_group(student2)
+    group.print_student_list()
     #group.load_students()
     #group.load_from_json()
     #new_students = group.STUDENTS
-    #group.print_student_list()
+
     #group.dump_students()
     #student.print_student(student.student)
