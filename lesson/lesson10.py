7cc957e01d250212adc3b0b24793c10188647916
dimon <xlxdimsanxlx@gmail.com>
2021-09-23 18:33:17 +0300

    two

---
+++ lesson/lesson10.py
@@ -0,0 +1,91 @@
+class Animal:
+    paws = 4
+    tail = True
+    ears = 2
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+    def voice(self):
+        print('Bzzzzzzzzz!!!')
+
+
+class Cat(Animal):
+
+    @staticmethod
+    def voice():
+        print('MEOW!!!!!!!')
+
+
+class Kitten(Cat):
+
+    def __init__(self, name, age):
+        super().__init__(name)
+        self.age = age
+
+
+class Dog(Animal):
+
+    weight = 0
+    TOTAL_COUNT = 0
+
+    def __init__(self, name='Jak', weight=15):
+        super().__init__(name)
+        self.weight = weight
+        Dog.TOTAL_COUNT += 1
+
+    def voice(self):
+        print('Bark! My name is {}'.format(self.name))
+
+    def __eq__(self, other):
+        if hasattr(other, 'weight'):
+            return self.weight == other.weight
+        else:
+            return NotImplemented
+
+    def __lt__(self, other):
+        return self.weight < other.weight
+
+    def __le__(self, other):
+        return self.weight <= other.weight
+
+    def __gt__(self, other):
+        return self.weight > other.weight
+
+    def __ge__(self, other):
+        return self.weight >= other.weight
+
+    @classmethod
+    def get_total_count(cls):
+        return cls.TOTAL_COUNT
+
+    @classmethod
+    def from_dict(cls, dict_data):
+        obj = cls()
+        for key in dict_data:
+            setattr(obj, key, dict_data[key])
+        return obj
+
+
+class CatDog(Dog, Cat):
+
+    def voice(self):
+        print('Meow! Bark!')
+
+
+if __name__ == '__main__':
+    #dog1 = Dog('Tuzik', 25)
+    #dog2 = Dog('Barbos', 30)
+    #dog1 = Dog()
+    #    print(dog1)
+    #    print(dog1.weight)
+    dog_data = {'name': 'Joy', 'weight': 15}
+    dog2 = Dog.from_dict(dog_data)
+    print(dog2)
+    print(dog2.weight)
+#    cat = Cat('Murzik')
+#    print(dog1 == cat)
+#    print(cat == dog1)
--- lesson/lms/students_oop.py
+++ lesson/lms/students_oop.py
@@ -1,31 +1,39 @@
-import json
 
 class Student:
 
-    student_fields = ['first_name', 'last_name', 'email', 'age', 'address', 'gender', 'point']
+    #student_fields = ['first_name', 'last_name', 'email', 'age', 'address', 'gender', 'point']
+    TOTAL_STUDENT = []
 
-    def __init__(self):
-        self. first_name = input(f'Enter {self.student_fields[0].replace("_", " ").capitalize()}:\t')
-        self. last_name = input(f'Enter {self.student_fields[1].replace("_", " ").capitalize()}:\t')
-        self. email = input(f'Enter {self.student_fields[2].replace("_", " ").capitalize()}:\t')
-        self.age = input(f'Enter {self.student_fields[3].replace("_", " ").capitalize()}:\t')
-        self. address = input(f'Enter {self.student_fields[4].replace("_", " ").capitalize()}:\t')
-        self.gender = input(f'Enter {self.student_fields[5].replace("_", " ").capitalize()}:\t')
-        self.point = input(f'Enter {self.student_fields[6].replace("_", " ").capitalize()}:\t')
+    def __init__(self, first_name, last_name, email, age, address, gender, point=0):
+        self. first_name = first_name
+        self. last_name = last_name
+        self. email = email
+        self.age = age
+        self. address = address
+        self.gender = gender
+        self.point = point
+        Student.TOTAL_STUDENT.append({self.first_name: self})
 
 
     def __str__(self):
         return f'{self.first_name.capitalize()} {self.last_name.capitalize()}'
 
+    def __eq__(self, other):
+        return self.age == other.age
+
+    @classmethod
+    def print_total_student(cls):
+        print(cls.TOTAL_STUDENT)
+
+    @classmethod
+    def add_student_dict(cls, dict_student):
+        obj = cls(first_name='', last_name='', email='', age=0, address='', gender='', point=0)
+        for key in dict_student:
+            setattr(obj, key, dict_student[key])
+        return obj
+
 
 class Group(Student):
-    '''
-    _TEST_STUDENTS = [
-        ['Mari', 'D', 'adadad@mail.ru', '19', 'Huston', 'F', 0],
-        ['Jon', 's', 'hyhyhyhyh@mail.com', '21', 'London', 'M', 0],
-        ['Andy', 'H', 'ggggggg@mail.com', 'sexteen', 'Citi', 'M', 0]
-    ]
-    '''
 
     def __init__(self, name):
         super()
@@ -35,38 +43,21 @@ class Group(Student):
     def add_student_to_group(self, student):
         self.STUDENTS.append(student)
 
-    '''
-    def load_students(self):
-        for test_student in self._TEST_STUDENTS:
-            self.STUDENTS.append(dict(zip(self.student_fields, test_student)))
-    '''
-
     def print_student_list(self):
         for student in self.STUDENTS:
             print(student)
             print('<-------------------------------------------------------->')
-    '''
-    def dump_students(self):
-        with open('data/student_data_oop.json', 'w') as file:
-            json.dump(self.STUDENTS, file)
 
-    def load_from_json(self, file_path='data/student_data_oop.json'):
-        with open(file_path, 'r') as file:
-            self.STUDENTS.extend(json.load(file))
-    '''
+
+
+#Обьект student1 создаетца и у него есть все аргументы но метод print_total_student почемуто не находит first_name
+
 
 if __name__ == '__main__':
-    student1 = Student()
-    student2 = Student()
-    #print(student)
-    print('-------------------------------------------')
-    group = Group('Beetroot')
-    group.add_student_to_group(student1)
-    group.add_student_to_group(student2)
-    group.print_student_list()
-    #group.load_students()
-    #group.load_from_json()
-    #new_students = group.STUDENTS
-
-    #group.dump_students()
-    #student.print_student(student.student)
+    student = Student('Mari', 'Boow', 'dadada@mail.com', 21, 'qwer', 'w')
+    #student1 = Student('Bob', 'Bown', 'dadada@mail.com', 25, 'asdasa', 'm')
+    dict_st = {'first_name': 'Bob', 'last_name': 'Bown', 'email': 'ghfhfghfg@gmail.com',
+               'age': 25, 'address': 'xcvvx', 'gender': 'm', 'point': 0}
+    student1 = Student.add_student_dict(dict_st)
+    print(student == student1)
+    Student.print_total_student()
