14bca6c5e57af0adf0d7aa18c01c9986de87b25f
dimon <xlxdimsanxlx@gmail.com>
2021-10-01 23:28:04 +0300

    two

--- homework/lesson13.py
+++ homework/lesson13.py
@@ -13,4 +13,25 @@ def hello_world():
     print('Hello World')
 
 
-hello_world()
+
+def my_decorator2(val, *args):
+    def internal_function(func):
+        def wrapper(*arg):
+            func(*arg)
+            for name in args:
+                func(name)
+            print(val, '\n__________________________________')
+            print(*args)
+        return wrapper
+    return internal_function
+
+
+@my_decorator2('Уря сработало!!!', 'World', 'Bob', 'Joi')
+def hello(name):
+    print(f'Hello {name}!')
+
+
+if __name__ == '__main__':
+    hello_world()
+    print('<--------------------------------------------------------------------------------------------------->')
+    hello('Eva')
