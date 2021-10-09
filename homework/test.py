d14f1901d0802b5165b9ee805c8acb5382b74e45
dimon <xlxdimsanxlx@gmail.com>
2021-10-09 13:24:22 +0300

    lesson

--- homework/test.py
+++ homework/test.py
@@ -1,27 +1,30 @@
-def include_list(list):
-    ansv = None
-    for item in list:
-        counts = list.count(item)
-        if counts != 1:
-            ansv = False
-            break
-        else:
-            ansv = True
-    return ansv
+import random
+
+plaers = ['Bob', 'Joi', 'Mik']
 
 
-# list1 = [1, 2, 3]
-# list2 = [1, 2, 3, 4, 4]
-# list3 = [1, 1, 2, 3]
-# print(include_list(list1))
-# print(include_list(list2))
-# print(include_list(list3))
+def play_raund():
+    point = 0
+    for item in range(3):
+        point += random.randint(1, 6)
+    return point
 
-def numbers(list):
-    nums = []
-    for item in list:
-        nums.append(-item)
-    return nums
+plaers_point = {}
+for plaer in plaers:
+    plaers_point[plaer] = play_raund()
 
-list1 = [1, 2, 3]
-print(numbers(list1))
+points = []
+for point in plaers_point.values():
+    points.append(point)
+
+max_point = max(points)
+
+vin_plaers = []
+for plaer, point in plaers_point.items():
+    if point == max_point:
+        vin_plaers.append({plaer: point})
+
+print(f'У нас {len(vin_plaers)} победител(я): ')
+for viner in vin_plaers:
+    for plaer, point in viner.items():
+        print(f'{plaer} point: {point}')
--- lesson/lesson17.py
+++ lesson/lesson17.py
@@ -1,9 +1,20 @@
+
+
+
+# Додати поле tag (тег) для Task, значення за замовчанням - None
+# У Dashboard додати можливість сортування за Task title
+# У Dashboard додати можливість пошуку задачі за Task tag
+# У Dashboard додати можливість вивести список завершених задач (done == True) по заданому tag
+
+
+
 class Task:
 
-    def __init__(self, title):
+    def __init__(self, title, tag=None):
         self.done = False
         self.title = title
         self._priority = 1
+        self.tag = tag
 
     def __str__(self):
         return f'{self.title.capitalize()}'
@@ -19,6 +30,9 @@ class Task:
         else:
             return 'Value out of range'
 
+
+
+# У Dashboard додати можливість вивести список завершених задач (done == True) по заданому tag
 class Dashboard:
     def __init__(self):
         self.task_list = []
@@ -30,7 +44,7 @@ class Dashboard:
 
     def print_all_tasks(self):
         for task in self.task_list:
-            return task
+            print(task)
 
     def print_tasks_by_priority(self):
         task_priority = []
@@ -44,20 +58,61 @@ class Dashboard:
 
         return task_priority
 
+    #У Dashboard додати можливість пошуку за Task title
+    def title_search(self, title):
+        title_list = []
+        for task in self.task_list:
+            if task.title == title:
+                title_list.append(task)
+        return title_list
+
+    #У Dashboard додати можливість пошуку задачі за Task tag
+    def tag_search(self, tag):
+        tag_list = []
+        for task in self.task_list:
+            if task.tag == tag:
+                tag_list.append(task)
+        return tag_list
+
+    #У Dashboard додати можливість вивести список завершених задач(done == True) по заданому tag
+    def completed_task(self, tag):
+
+        t_list = Dashboard.tag_search(self, tag)
+
+        completed_list = []
+        for task in t_list:
+            if task.done == True:
+                completed_list.append(task)
+        return completed_list
+
+
+
+
 
 
 
 if __name__ == '__main__':
     task = Task('My test task')
+    task.done = True
+    task1 = Task('My test task1', '#task1')
+    task1.done = True
+    task2 = Task('My test task2', '#task1')
+    task3 = Task('My test task3', '#task3')
+    task3.done = True
+    task4 = Task('My test task4', '#task2')
     dashboard = Dashboard()
-    dashboard.add_task()
-    dashboard.task_list[-1].priority = 2
-    print(dashboard.task_list[-1].priority)
-    dashboard.add_task()
-    dashboard.task_list[-1].priority = 2
-    dashboard.add_task()
-    dashboard.task_list[-1].priority = 4
-    print(dashboard.print_tasks_by_priority())
+    dashboard.task_list.extend([task, task1, task2, task3, task4])
+    for task in dashboard.completed_task('#task1'):
+        print(task)
+
+    # dashboard.add_task()
+    # dashboard.task_list[-1].priority = 2
+    # print(dashboard.task_list[-1].priority)
+    # dashboard.add_task()
+    # dashboard.task_list[-1].priority = 2
+    # dashboard.add_task()
+    # dashboard.task_list[-1].priority = 4
+    # print(dashboard.print_tasks_by_priority())
     #dashboard.print_all_tasks()
     #task.priority = 11
     #print(task._priority)
